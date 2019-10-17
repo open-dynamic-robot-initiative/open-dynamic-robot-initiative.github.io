a3ed5f7ead001d81ccacb0f2dccd8b6ef1c0d746
Bamboo build agent <bambooagent@sow-oldlady3.is.localnet>
2019-10-17 14:36:16 +0200

    [bamboo blmc-robots] update documentation

--- code_documentation/robot_interfaces/classrobot__interfaces_1_1RobotBackend.js
+++ code_documentation/robot_interfaces/classrobot__interfaces_1_1RobotBackend.js
@@ -3,7 +3,7 @@ var classrobot__interfaces_1_1RobotBackend =
     [ "Status", "structrobot__interfaces_1_1RobotBackend_1_1Status.html", "structrobot__interfaces_1_1RobotBackend_1_1Status" ],
     [ "RobotBackend", "classrobot__interfaces_1_1RobotBackend.html#a3120d5df6b7b64fa9bbcf229cf172a39", null ],
     [ "~RobotBackend", "classrobot__interfaces_1_1RobotBackend.html#af43d9fbf6fcac1e37d8fc6196440dec5", null ],
-    [ "get_max_action_repetitions", "classrobot__interfaces_1_1RobotBackend.html#a6f9cd568f7be525afb04cb4b9f319125", null ],
+    [ "get_max_action_repetitions", "classrobot__interfaces_1_1RobotBackend.html#ae354dfd960d4fd0d2f9242dcfb4a701f", null ],
     [ "initialize", "classrobot__interfaces_1_1RobotBackend.html#a7e4eb9f5362b79c0c21b824e3b639ae6", null ],
-    [ "set_max_action_repetitions", "classrobot__interfaces_1_1RobotBackend.html#ae84c015bb3ca65a00702b7fbf56f14bc", null ]
+    [ "set_max_action_repetitions", "classrobot__interfaces_1_1RobotBackend.html#aad761d1e0ab7296a9632b9c4cc9c91db", null ]
 ];
--- code_documentation/robot_interfaces/classrobot__interfaces_1_1RobotData.js
+++ code_documentation/robot_interfaces/classrobot__interfaces_1_1RobotData.js
@@ -1,7 +1,7 @@
 var classrobot__interfaces_1_1RobotData =
 [
     [ "Ptr", "classrobot__interfaces_1_1RobotData.html#a73dad807aaccab42802c674022a6e3a0", null ],
-    [ "RobotData", "classrobot__interfaces_1_1RobotData.html#a6192658b55fac32904f270baf73ad6e0", null ],
+    [ "RobotData", "classrobot__interfaces_1_1RobotData.html#a5e9b05cf78521359f0d4229c7d035b0e", null ],
     [ "applied_action", "classrobot__interfaces_1_1RobotData.html#aec5eb3582eb4e791a7bfefe4bae38526", null ],
     [ "desired_action", "classrobot__interfaces_1_1RobotData.html#a1a6d8193012e1167ce725d9e2c8934fc", null ],
     [ "observation", "classrobot__interfaces_1_1RobotData.html#a932c50df98ed3287ce8b6cd35f78bfc4", null ],
