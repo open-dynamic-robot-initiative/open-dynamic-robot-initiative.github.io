67f81e36bb8005a5352bbc99dba673dd703d2adf
Bamboo build agent <bambooagent@sow-oldlady3.is.localnet>
2020-10-05 15:16:48 +0200

    [bamboo BLMC EI] update documentation

--- code_documentation/trifinger_simulation/docs/doxygen/html/search/all_7.js
+++ code_documentation/trifinger_simulation/docs/doxygen/html/search/all_7.js
@@ -7,7 +7,6 @@ var searchData=
   ['get_5fdesired_5faction',['get_desired_action',['../classtrifinger__simulation_1_1sim__finger_1_1SimFinger.html#a1a808eb19a1a9a13a4c0fe4f2d07d26c',1,'trifinger_simulation::sim_finger::SimFinger']]],
   ['get_5fimage',['get_image',['../classtrifinger__simulation_1_1camera_1_1Camera.html#a9c614de55e1408fa131d9f30a86759be',1,'trifinger_simulation::camera::Camera']]],
   ['get_5fimages',['get_images',['../classtrifinger__simulation_1_1camera_1_1TriFingerCameras.html#ab87b4d284e9a42deebe874ad5fb233b9',1,'trifinger_simulation::camera::TriFingerCameras']]],
-  ['get_5fobject_5fpose',['get_object_pose',['../classtrifinger__simulation_1_1trifinger__platform_1_1TriFingerPlatform.html#ad46ed81c3e6c557c9cce2ce919b6110b',1,'trifinger_simulation::trifinger_platform::TriFingerPlatform']]],
   ['get_5fobservation',['get_observation',['../classtrifinger__simulation_1_1real__finger_1_1RealFinger.html#a5e10f8a9627376f072ad2ea64ecdd361',1,'trifinger_simulation::real_finger::RealFinger']]],
   ['get_5ftime_5fstep',['get_time_step',['../classtrifinger__simulation_1_1trifinger__platform_1_1TriFingerPlatform.html#ada1b2f4cbf62c8fe0cd36617b0935786',1,'trifinger_simulation::trifinger_platform::TriFingerPlatform']]],
   ['get_5ftimestamp_5fms',['get_timestamp_ms',['../classtrifinger__simulation_1_1sim__finger_1_1SimFinger.html#a7836fb9b5deacefa636afcea7b9639d7',1,'trifinger_simulation::sim_finger::SimFinger']]],
